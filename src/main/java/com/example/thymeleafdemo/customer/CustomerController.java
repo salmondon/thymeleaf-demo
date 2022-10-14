c920a924b2ad0733038917cfef837d854902db3b
Max <onlyyousunday@gmail.com>
2022-10-14 20:58:20 +0700

    update

--- src/main/java/com/example/thymeleafdemo/customer/CustomerController.java
+++ src/main/java/com/example/thymeleafdemo/customer/CustomerController.java
@@ -1,8 +1,13 @@
 package com.example.thymeleafdemo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class CustomerController {
@@ -12,5 +17,15 @@ public class CustomerController {
     @GetMapping("/customer/all")
     public Iterable<Customer> getCustomerList() {
         return customerRepository.findAll();
+    }
+
+    @GetMapping(path = "/customer/{id}")
+    public ResponseEntity<Customer> getById(@PathVariable(value = "id") long id) {
+        Optional<Customer> response = customerRepository.findById(id);
+        if (response.isPresent()) {
+            return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 }
